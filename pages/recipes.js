ada081088a9e66a4cd9d7b2569d81f1ac2efaa9f
MessingAround-test <62684645+MessingAround-test@users.noreply.github.com>
2022-04-17 13:41:42 +1000

    vw

--- pages/recipes.js
+++ pages/recipes.js
@@ -73,7 +73,7 @@ export default function Home() {
                     {recipes.map((recipe) => {
                         return (
                             
-                            <Card style={{ maxWidth: '30em', minWidth:"30em", maxHeight: "30em", minHeight: "30em", color: "black"}} onClick={()=>(redirect("/recipes/" + recipe._id))}>
+                            <Card style={{ maxWidth: '30vw', minWidth:"30vw", maxHeight: "30vw", minHeight: "30vw", color: "black"}} onClick={()=>(redirect("/recipes/" + recipe._id))}>
                                 <Card.Body style={{overflow: "hidden"}}>
                                     <Card.Title>{String(recipe.name)}</Card.Title>
                                     <Card.Img variant="top" src={recipe.image} />
