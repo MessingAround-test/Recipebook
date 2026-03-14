f5ee22cbc9ee1a3a6b40999352cd6d2049958bcb
MessingAround-test <62684645+MessingAround-test@users.noreply.github.com>
2026-03-14 15:37:08 +1100

    PWA changes?

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -20,7 +20,13 @@ export function Layout({ title, description = 'Premium Culinary Management', chi
                     <title>{`${title} | Recipebook`}</title>
                     <meta name="description" content={description} />
                     <link rel="icon" href="/avo.ico" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <meta name="theme-color" content="#ffffff" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+                    <meta name="apple-mobile-web-app-title" content="Recipebook" />
                 </Head>
+
 
                 <main className={styles.main}>
                     {children}
